3edb0100b3eee7bea9a59662c089463c8451e9c4
Gerankin, Andrey (C) <andrey.gerankin@csaa.com>
2019-01-04 11:53:40 +0300

    added relay

--- my_garage/my_garage.ino
+++ my_garage/my_garage.ino
@@ -2,45 +2,65 @@
 #include <DHT_U.h>
 
 /*####################################################################
- Kostas Tsirogiannis
- tsirogk@gmail.com
- 
- I recombined two example sketches to make 
- Arduino Yun display temp and humidity to LCD (and optionally serial)
- Made from a combination of original dht11 library and the blog 
- http://www.geeetech.com/wiki/index.php/Electric_thermometer_by_using_DHT11_sensor_module
- 
- BOARD -> ARDUINO YUN
+ BOARD -> ARDUINO NANO
  sensor www.microbot.it temp+humidity 
  * +5V   -> 5V
  * GND   -> GND
  * DATA  -> digital pin 8
 
- HISTORY:
- Mirko Prosseda - Original version (21/07/2013)
 #######################################################################*/
 
+//switch on vent on 
+int humidity_on = 80;
+//switch off vent on 
+int humidity_off = 40;
+//switch on hotter on 
+int temper_on = 4;
+//switch off hotter on 
+int temper_off = 8;
+unsigned int gettemp=0;
 
-//#include <LiquidCrystal.h>
 #include <SimpleDHT.h>
 int pinDHT11 = 8;
 SimpleDHT11 dht11(pinDHT11);
-//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+#define RELAY_HOT 2
+#define RELAY_VENT 3
+int states [RELAY_VENT+1] = {HIGH, HIGH,HIGH, HIGH};
 
 void setup()
-
 {
+  pinMode(RELAY_VENT, OUTPUT); 
+  pinMode(RELAY_HOT, OUTPUT);
+    relayOn(RELAY_VENT);
+    relayOn(RELAY_HOT);
+  
   Serial.begin(9600);
   delay(1000);          // wait for sensor initialization
-//lcd.begin(16, 2);
+    while(!Serial){
+      ; // wait for serial port to connect. Needed for native USB port only
+    }
+
+  Serial.println("\n[memCheck]");
+  Serial.println(freeRam());
+
+    relayOff(RELAY_VENT);
+    relayOff(RELAY_HOT);
 }
 
 void loop()
 {
-  Serial.print("Sensor status: ");
-  readTemp();
-
-  delay(2500);
+   // count 60 seconds
+   if(millis()/60000!=gettemp) {  
+    gettemp=millis()/60000;
+    //log(readDHT());
+    if(gettemp %2 == 0) {
+      checkHumidity();
+    }
+    else {
+      checkTemper();
+    }
+   }
 }
 
 void readTemp(){
@@ -61,4 +81,96 @@ void readTemp(){
   Serial.print((int)humidity); Serial.println(" H");
   
   // DHT11 sampling rate is 1HZ.
+}
+void relayOn(int relay){
+  if(states[relay] != LOW){
+    digitalWrite(relay, LOW);  //switch ON
+    states[relay] = LOW;
+    String msg = String("relay switch on ");
+    msg += relay;
+    log(msg);
+  }
+}
+
+void relayOff(int relay){
+  if(states[relay] != HIGH){
+    digitalWrite(relay, HIGH);  // switch OFF
+    states[relay] = HIGH;
+    String msg = String("relay switch off ");
+    msg += relay;
+    log(msg);
+  }
+}
+
+void log(String msg){
+       //File myFile = SD.open("temper.log", FILE_WRITE);
+       String record="";//get_time();
+       record+=(" " + msg);
+       Serial.println(record);
+       //myFile.println(record);
+       //myFile.close();
+}
+
+void checkHumidity(){
+  int currentValue = getHumidity();
+  if(currentValue >= humidity_on) {
+    relayOn(RELAY_VENT);
+  }
+  if(currentValue <= humidity_off){
+    relayOff(RELAY_VENT);
+  }
+}
+
+void checkTemper(){
+  int currentValue = getTemper();
+  if(currentValue <= temper_on) {
+    relayOn(RELAY_HOT);
+  }
+  if(currentValue >= temper_off){
+    relayOff(RELAY_HOT);
+  }
+}
+
+ 
+int getHumidity(){
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // read without samples.
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    return 0;
+  }
+  String msg = String("humidity is ");
+  //************** DEBUG ONLY
+  humidity = random(40)+60;
+  msg += humidity;
+  log(msg);
+  return humidity;
+}
+
+int getTemper(){
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // read without samples.
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    return 100;
+  }
+  String msg = String("temperature is ");
+  //************** DEBUG ONLY
+  temperature = random(10);
+  msg += temperature;
+  log(msg);
+  return temperature;
+}
+int freeRam () {
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
 }
