f160fd16e37a94c76c4cd680f0f126f9b0e955ba
Gerankin, Andrey (C) <andrey.gerankin@csaa.com>
2019-01-05 20:29:40 +0300

    added sd

--- my_garage/my_garage.ino
+++ my_garage/my_garage.ino
@@ -1,24 +1,26 @@
 #include <DHT.h>
 #include <DHT_U.h>
+#include <SD.h>
+//#include <Wire.h>
 
 /*####################################################################
- BOARD -> ARDUINO NANO
- sensor www.microbot.it temp+humidity 
- * +5V   -> 5V
- * GND   -> GND
- * DATA  -> digital pin 8
+  BOARD -> ARDUINO NANO
+  sensor www.microbot.it temp+humidity
+   +5V   -> 5V
+   GND   -> GND
+   DATA  -> digital pin 8
 
-#######################################################################*/
+  #######################################################################*/
 
-//switch on vent on 
+//switch on vent on
 int humidity_on = 80;
-//switch off vent on 
+//switch off vent on
 int humidity_off = 40;
-//switch on hotter on 
+//switch on hotter on
 int temper_on = 4;
-//switch off hotter on 
+//switch off hotter on
 int temper_off = 8;
-unsigned int gettemp=0;
+unsigned int gettemp = 0;
 
 #include <SimpleDHT.h>
 int pinDHT11 = 8;
@@ -26,64 +28,132 @@ SimpleDHT11 dht11(pinDHT11);
 
 #define RELAY_HOT 2
 #define RELAY_VENT 3
-int states [RELAY_VENT+1] = {HIGH, HIGH,HIGH, HIGH};
+int states [RELAY_VENT + 1] = {HIGH, HIGH, HIGH, HIGH};
 
+#define SD_CS 10
+String iniFile = "settings.ini";
+String logFile;
 void setup()
 {
-  pinMode(RELAY_VENT, OUTPUT); 
+  logFile = "data";
+  logFile += analogRead(0);
+  logFile += ".log";
+  pinMode(RELAY_VENT, OUTPUT);
   pinMode(RELAY_HOT, OUTPUT);
-    relayOn(RELAY_VENT);
-    relayOn(RELAY_HOT);
   
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Current log file is " + logFile);
+  relayOn(RELAY_VENT);
+  relayOn(RELAY_HOT);
   delay(1000);          // wait for sensor initialization
-    while(!Serial){
-      ; // wait for serial port to connect. Needed for native USB port only
-    }
 
-  Serial.println("\n[memCheck]");
+  Serial.print("\n[memCheck]");
   Serial.println(freeRam());
 
-    relayOff(RELAY_VENT);
-    relayOff(RELAY_HOT);
+  relayOff(RELAY_VENT);
+  relayOff(RELAY_HOT);
+  pinMode(SD_CS, OUTPUT);
+  if (!SD.begin(SD_CS)) {
+    Serial.println("initialization SD failed!");
+    return;
+  }
+  Serial.println("initialization SD done.");
+  readSettings();
 }
 
 void loop()
 {
-   // count 60 seconds
-   if(millis()/60000!=gettemp) {  
-    gettemp=millis()/60000;
+  // count 60 seconds
+  if (millis() / 60000 != gettemp) {
+    gettemp = millis() / 60000;
     //log(readDHT());
-    if(gettemp %2 == 0) {
+    if (gettemp % 2 == 0) {
       checkHumidity();
     }
     else {
       checkTemper();
     }
-   }
+  }
+
+}
+void readSettings(){
+    if (checkSettings()) {
+      File confFile = SD.open(iniFile);
+      Serial.println(iniFile + " : ");
+
+      humidity_on = readInt(confFile);
+      humidity_off = readInt(confFile);
+      String hum = String("humidity on ");
+      hum += humidity_on;
+      hum += " off ";
+      hum += humidity_off;
+      Serial.println(hum);
+
+      temper_on = readInt(confFile);
+      temper_off = readInt(confFile);
+      confFile.close();
+      String hot = String("hotter on ");
+      hot += temper_on;
+      hot += " off ";
+      hot += temper_off;
+      Serial.println(hot);
+    }
+}
+int readInt(File file){
+  int k = 0;
+  int num2 = 0;
+  while(file.available()){
+    char s = file.read();
+    if(s >= 48 && s <= 57){
+      k++;
+      num2 = num2 * 10 + s-'0';
+    }
+    else if(k > 0) {
+      break;
+    }
+  }
+  String result = String();
+  result += "-";
+  result += num2;
+  result += "-";
+  Serial.println(result);
+  return num2;
 }
 
-void readTemp(){
- Serial.println("=================================");
+bool checkSettings(){
+  Serial.print(iniFile);
+  if (SD.exists(iniFile)) 
+       {
+       Serial.println(" file exists.");
+       return true;
+       } 
+   Serial.println(" file not exists.");
+   return false;
+}
+void readTemp() {
+  Serial.println("=================================");
   Serial.println("Sample DHT11...");
-  
+
   // read without samples.
   byte temperature = 0;
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
   if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    Serial.print("Read DHT11 failed, err="); Serial.println(err); delay(1000);
     return;
   }
-  
+
   Serial.print("Sample OK: ");
-  Serial.print((int)temperature); Serial.print(" *C, "); 
+  Serial.print((int)temperature); Serial.print(" *C, ");
   Serial.print((int)humidity); Serial.println(" H");
-  
+
   // DHT11 sampling rate is 1HZ.
 }
-void relayOn(int relay){
-  if(states[relay] != LOW){
+void relayOn(int relay) {
+  if (states[relay] != LOW) {
     digitalWrite(relay, LOW);  //switch ON
     states[relay] = LOW;
     String msg = String("relay switch on ");
@@ -92,8 +162,8 @@ void relayOn(int relay){
   }
 }
 
-void relayOff(int relay){
-  if(states[relay] != HIGH){
+void relayOff(int relay) {
+  if (states[relay] != HIGH) {
     digitalWrite(relay, HIGH);  // switch OFF
     states[relay] = HIGH;
     String msg = String("relay switch off ");
@@ -102,37 +172,37 @@ void relayOff(int relay){
   }
 }
 
-void log(String msg){
-       //File myFile = SD.open("temper.log", FILE_WRITE);
-       String record="";//get_time();
-       record+=(" " + msg);
-       Serial.println(record);
-       //myFile.println(record);
-       //myFile.close();
+void log(String msg) {
+  File myFile = SD.open(logFile, FILE_WRITE);
+  String record = ""; //get_time();
+  record += (" " + msg);
+  Serial.println(record);
+  myFile.println(record);
+  myFile.close();
 }
 
-void checkHumidity(){
+void checkHumidity() {
   int currentValue = getHumidity();
-  if(currentValue >= humidity_on) {
+  if (currentValue >= humidity_on) {
     relayOn(RELAY_VENT);
   }
-  if(currentValue <= humidity_off){
+  if (currentValue <= humidity_off) {
     relayOff(RELAY_VENT);
   }
 }
 
-void checkTemper(){
+void checkTemper() {
   int currentValue = getTemper();
-  if(currentValue <= temper_on) {
+  if (currentValue <= temper_on) {
     relayOn(RELAY_HOT);
   }
-  if(currentValue >= temper_off){
+  if (currentValue >= temper_off) {
     relayOff(RELAY_HOT);
   }
 }
 
- 
-int getHumidity(){
+
+int getHumidity() {
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   // read without samples.
@@ -140,18 +210,18 @@ int getHumidity(){
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
   if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    Serial.print("Read DHT11 failed, err="); Serial.println(err); delay(1000);
     return 0;
   }
   String msg = String("humidity is ");
   //************** DEBUG ONLY
-  humidity = random(40)+60;
+  humidity = random(40) + 60;
   msg += humidity;
   log(msg);
   return humidity;
 }
 
-int getTemper(){
+int getTemper() {
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   // read without samples.
@@ -159,7 +229,7 @@ int getTemper(){
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
   if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    Serial.print("Read DHT11 failed, err="); Serial.println(err); delay(1000);
     return 100;
   }
   String msg = String("temperature is ");
@@ -170,7 +240,7 @@ int getTemper(){
   return temperature;
 }
 int freeRam () {
-  extern int __heap_start, *__brkval; 
-  int v; 
-  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+  extern int __heap_start, *__brkval;
+  int v;
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
